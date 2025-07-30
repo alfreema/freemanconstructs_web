05268ae71908a7c06bee955ec4b999c730712d81
Aaron Freeman <aaron@sendthisfile.com>
2025-07-30 16:23:22 -0500

    Cleanup unnecessary and obsoleted files

--- layer-stack/layer-stack-model.js
+++
@@ -1,7 +0,0 @@
-export function computeStackLayout(count, spacing = 10, baseY = 0) {
-    return Array.from({ length: count }, (_, i) => ({
-      yOffset: baseY + i * spacing,
-      zIndex: i,
-    }));
-  }
-  
